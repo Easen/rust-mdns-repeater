7f96b51816cbf60500c970f1bf6ca5fc40ae8dde
Marc Easen <marc@easen.co.uk>
2024-02-15 22:49:02 +0000

    feat: forwards mdns packets

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,19 @@
-#![allow(dead_code)]
-#![allow(unused_assignments)]
-#![allow(unused_variables)]
-#![allow(unused_imports)]
-use std::error::Error;
-use std::mem;
-use std::net::Ipv4Addr;
-use std::os::fd::{AsFd, AsRawFd, OwnedFd};
-
 use clap::Parser;
-use log::{debug, error, info};
-use std::ffi::OsString;
-// use nix::sys::epoll::*;
 use env_logger::Env;
 use ipnet::Ipv4Net;
-use nix::ioctl_read;
-use nix::libc::{c_char, ifreq, in_addr, SIOCGIFADDR, SIOCGIFNETMASK};
+use log::{debug, error, info};
+use nix::libc::{c_char, ifreq, SIOCGIFADDR, SIOCGIFNETMASK};
+use nix::sys::epoll::*;
 use nix::sys::socket::sockopt::{
     BindToDevice, IpAddMembership, IpMulticastLoop, Ipv4PacketInfo, ReuseAddr,
 };
 use nix::sys::socket::*;
-use nix::*;
+use std::error::Error;
+use std::ffi::OsString;
+use std::mem;
+use std::net::Ipv4Addr;
+use std::os::fd::RawFd;
+use std::os::fd::{AsRawFd, OwnedFd};
 
 pub type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
@@ -29,7 +23,6 @@ const MDNS_ADDR: Ipv4Addr = Ipv4Addr::new(224, 0, 0, 251);
 nix::ioctl_read_bad!(siocgifaddr, SIOCGIFADDR, ifreq);
 nix::ioctl_read_bad!(siocgifnetmask, SIOCGIFNETMASK, ifreq);
 
-/// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
@@ -43,8 +36,8 @@ struct Args {
 
 #[derive(Debug)]
 struct Interface {
-    name: String,
-    network: Ipv4Net,
+    pub name: String,
+    pub network: Ipv4Net,
     sockfd: OwnedFd,
 }
 
@@ -66,7 +59,7 @@ fn sockaddr_to_ipv4addr(addr: sockaddr) -> Result<Ipv4Addr> {
 }
 
 impl Interface {
-    fn new(interface: &String) -> Result<Self> {
+    fn new(interface_name: &String) -> Result<Self> {
         let sock_fd = socket(
             AddressFamily::Inet,
             SockType::Datagram,
@@ -74,32 +67,43 @@ impl Interface {
             SockProtocol::Udp,
         )?;
 
-        setsockopt(&sock_fd, BindToDevice, &OsString::from(&interface))?;
+        setsockopt(&sock_fd, BindToDevice, &OsString::from(&interface_name))?;
 
-        let mut req = ifreq_for(interface);
+        let mut req = ifreq_for(interface_name);
         let addr: Ipv4Addr;
         unsafe {
+            // get the ipv4 address
             siocgifaddr(sock_fd.as_raw_fd(), &mut req)?;
             addr = sockaddr_to_ipv4addr(req.ifr_ifru.ifru_addr)?;
         };
 
         let mask: Ipv4Addr;
         unsafe {
+            // get the ipv4 mask
             siocgifnetmask(sock_fd.as_raw_fd(), &mut req)?;
             mask = sockaddr_to_ipv4addr(req.ifr_ifru.ifru_addr)?;
         };
 
-        let network = Ipv4Net::with_netmask(addr, mask)?;
+        // add interface to the multicast-group
+        setsockopt(
+            &sock_fd,
+            IpAddMembership,
+            &IpMembershipRequest::new(MDNS_ADDR, Some(addr)),
+        )?;
 
         Ok(Interface {
-            name: interface.clone(),
-            network,
+            name: interface_name.clone(),
+            network: Ipv4Net::with_netmask(addr, mask)?,
             sockfd: sock_fd,
         })
     }
+
+    fn network_contains_addr(&self, other: Ipv4Addr) -> bool {
+        self.network.contains(&other)
+    }
 }
 
-fn create_receiving_socket(interfaces: Vec<Interface>) -> Result<OwnedFd> {
+fn create_receiving_socket() -> Result<OwnedFd> {
     // create a UDP socket
     let recv_fd = socket(
         AddressFamily::Inet,
@@ -108,6 +112,9 @@ fn create_receiving_socket(interfaces: Vec<Interface>) -> Result<OwnedFd> {
         SockProtocol::Udp,
     )?;
 
+    // reuse the address
+    setsockopt(&recv_fd, ReuseAddr, &true)?;
+
     // bind the 0.0.0.0:5353
     let addr = SockaddrIn::new(0, 0, 0, 0, MDNS_PORT);
     bind(recv_fd.as_raw_fd(), &addr)?;
@@ -115,19 +122,13 @@ fn create_receiving_socket(interfaces: Vec<Interface>) -> Result<OwnedFd> {
     // enable loopback, just in case someone else needs to the data
     setsockopt(&recv_fd, IpMulticastLoop, &true)?;
 
-    for interface in interfaces {
-        setsockopt(
-            &recv_fd,
-            IpAddMembership,
-            &IpMembershipRequest::new(MDNS_ADDR, Some(interface.network.addr())),
-        )?;
-    }
+    setsockopt(&recv_fd, Ipv4PacketInfo, &true)?;
 
     Ok(recv_fd)
 }
 
 fn main() -> Result<()> {
-    let env = Env::default().filter_or("LOG", "info");
+    let env = Env::default().filter_or("RUST_LOG", "info");
     env_logger::init_from_env(env);
 
     let args = Args::parse();
@@ -137,18 +138,94 @@ fn main() -> Result<()> {
     }
 
     info!("Setting up the interfaces");
-    let interfaces: Vec<_> = args
+    let interfaces = args
         .interfaces
         .iter()
-        .map(|inter| Interface::new(inter).unwrap())
-        .collect();
+        .map(|interface_name| match Interface::new(interface_name) {
+            Ok(interface) => {
+                info!(
+                    "interface {:?}: network {:?}",
+                    interface_name, interface.network
+                );
+                return interface;
+            }
+            Err(err) => panic!(
+                "Error occurred while establishing interface {:?} - {:?}",
+                interface_name,
+                err.to_string()
+            ),
+        })
+        .collect::<Vec<Interface>>();
 
     info!("Creating receiving socket");
-    let recv_fd = create_receiving_socket(interfaces)?;
+    let recv_fd = match create_receiving_socket() {
+        Ok(recv_fd) => recv_fd,
+        Err(err) => {
+            error!("Unable to create receiving socket - {:?}", err);
+            return Err(err);
+        }
+    };
 
     info!("Setting up epoll");
+    let epoll = Epoll::new(EpollCreateFlags::empty())?;
+    let mut epoll_events = vec![EpollEvent::empty(); 16];
+    let event = EpollEvent::new(EpollFlags::EPOLLIN, recv_fd.as_raw_fd() as u64);
+    epoll.add(&recv_fd, event)?;
 
-    Ok(())
+    info!("Starting poll...");
+    let dst: SockaddrIn = SockaddrIn::new(224, 0, 0, 251, MDNS_PORT);
+    loop {
+        let num = epoll.wait(&mut epoll_events, 100)?;
+
+        'events: for i in 0..num {
+            let mut buf: [u8; 4096] = [0; 4096];
+            let sockfd = epoll_events[i].data() as RawFd;
+            let (len, addr) = recvfrom::<SockaddrIn>(sockfd, &mut buf)?;
+
+            if addr.is_none() {
+                continue 'events;
+            }
+            let addr = Ipv4Addr::from(addr.unwrap().ip());
+
+            // check for loopback
+            let loop_back_interface = &interfaces.iter().find(|x| x.network.addr() == addr);
+            if loop_back_interface.is_some() {
+                debug!("Ignoring loopback a MDNS packet from {:?}", addr);
+                continue 'events;
+            }
+
+            let src_interface = interfaces
+                .iter()
+                .find(|interface| interface.network_contains_addr(addr));
+
+            if src_interface.is_none() {
+                debug!(
+                    "Ignoring a MDNS packet from an unknown interface from {:?}",
+                    addr
+                );
+                continue 'events;
+            }
+            let src_interface = src_interface.unwrap();
+
+            debug!(
+                "Received MDNS packets from {:?} from {:?} (sockfd: {})",
+                addr, src_interface.name, sockfd
+            );
+
+            interfaces
+                .iter()
+                .filter(|interface| !interface.name.eq(&src_interface.name))
+                .for_each(|interface| {
+                    match sendto(interface.sockfd.as_raw_fd(), &buf[0..len], &dst, MsgFlags::empty()) {
+                        Err(err) => {
+                            error!("Unable to forward MDNS packets from {:?} to {:?} due to error - {:?}",  addr, interface.name, err)
+                        }
+                        Ok(_) => info!(
+                            "Forwared MDNS packets from {:?} to {:?}",
+                            addr, interface.name
+                        ),
+                    }
+                });
+        }
+    }
 }
-
-
